e25c8ff6b7ad7f5842414926bc08793c6b080a07
wykim <ksq111@naver.com>
2026-05-10 18:09:54 +0000

    write timer example code

--- concurrency_lab/timer/example/timer_main.c
+++ concurrency_lab/timer/example/timer_main.c
@@ -1,3 +1,8 @@
+/*
+timer_thread: 만료 감지 → cond_signal
+                                ↓
+worker_thread: cond_wait로 대기 중 → 깨어남 → task 수행 → 다시 cond_wait
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,7 +11,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <sys/time.h>
-#include <sys/types.h>ßßßßß
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
@@ -16,4 +21,23 @@
 // 뮤텍스, cond 초기화
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+
+
+void *timer_thread(void *arg) {
+    return NULL;
+}
+
+void *worker_thread(void *arg) {
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    
+    pthread_t timer_thread_id, worker_thread_id;
+
+    
+    
+    return 0;
+}
 
